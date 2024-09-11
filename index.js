8e6291f8126a11b9f3346314154da34d8561e19e
James Bart <jbart1@gmail.com>
2024-09-11 23:29:15 +0100

    Working on part3

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+const cors = require('cors')
+app.use(cors({ origin: 'http://localhost:5173' }));
 const morgan = require('morgan')
+
 
 morgan.token('body', req => {
   return JSON.stringify(req.body)
@@ -11,71 +14,76 @@ app.use(morgan(':method :url :status :response-time ms :body'))
 
 var currentDate = new Date(); 
 var currentDateString = currentDate.toString();
-
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: "1",
-  },
-  { 
-      name: "Ada Lovelace", 
-      number: "39-44-5323523",
-      id: "2"
-  },
-  { 
-      name: "Dan Abramov", 
-      number: "12-43-234345",
-      id: "3"
-  },
-  { 
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122",
-      id: "4"
-  }
-]
+  let notes = [
+    {
+      id: "1",
+      content: "HTML is easy",
+      important: false
+    },
+    {
+      id: "2",
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: "3",
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: false
+    },
+    {
+      id: "6",
+      content: "sdsda\\dd",
+      important: true
+    },
+    {
+      id: "8",
+      content: "fcsccccz\\c",
+      important: true
+    }
+  ]
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} persons </br> ${currentDateString}`); 
+    response.send(`Phonebook has info for ${notes.length} notes </br> ${currentDateString}`); 
 })
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response) => {
   id = request.params.id
-  const person = persons.find(person =>person.id ==id)
-  if (person) {
-    response.json(person)
+  const note = notes.find(note=>note.id ==id)
+  if (note) {
+    response.json(note)
   } else{
         response.status(404).end()
   }
 
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response) => {
 
     id = request.params.id
-    persons = persons.filter(person=>person.id != id)
+    notes = notes.filter(note=>note.id != id)
     response.status(204).end()
 
 })
 
 const generateID = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => Number(n.id))) 
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => Number(n.id))) 
         : 0
     return String(maxId + 1)
 }
 
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/notes', (request, response) => {
     const body = request.body
+    console.log(body)
     if (!body) {
         return response.status(400).json({ 
             error: 'The name or number are missing' 
@@ -83,25 +91,25 @@ app.post('/api/persons', (request, response) => {
     }
 
 
-    const person = {
-        name: body.name,
-        number:body.number,
+    const note = {
         id: generateID(),
+        content: body.content,
+        important:body.important,
     }
 
-    const duplicate = persons.find(personcheck =>personcheck.name == person.name)
+    const duplicate = notes.find(notecheck =>notecheck.name ==note.content)
     if (duplicate) {
         return response.status(400).json({ 
-            error: `The name ${person.name} already exists` 
+            error: `The name ${note.name} already exists` 
         })
     }
 
-    persons = persons.concat(person)
-    response.json(person)
+    notes = notes.concat(note)
+    response.json(note)
 
 })
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+//const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
